c5d682531ec3d52676c2a94e220937830ab41817
Xon <tazipper1@hotmail.com>
2017-09-04 17:22:36 +0800

    Store _output not _data

---
+++ upload/src/addons/SV/RedisViewCounter/_output/extension_hint.php
@@ -0,0 +1,10 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace SV\RedisViewCounter\XF\Repository
+{
+	class XFCP_Attachment extends \XF\Repository\Attachment {}
+	class XFCP_Thread extends \XF\Repository\Thread {}
+}
