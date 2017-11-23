9f71bb5762a29ab9334fef67938427cfd42c04f4
Xon <tazipper1@hotmail.com>
2017-11-23 12:45:44 +0800

    Block installing if Redis is not installed & configured

---
+++ upload/src/addons/SV/RedisViewCounter/Setup.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace SV\UserActivity;
+
+use XF\AddOn\AbstractSetup;
+use XF\AddOn\StepRunnerInstallTrait;
+use XF\AddOn\StepRunnerUninstallTrait;
+use XF\AddOn\StepRunnerUpgradeTrait;
+use SV\RedisCache\Redis;
+
+/**
+ * Add-on installation, upgrade, and uninstall routines.
+ */
+class Setup extends AbstractSetup
+{
+    use StepRunnerInstallTrait;
+    use StepRunnerUpgradeTrait;
+    use StepRunnerUninstallTrait;
+
+    public function checkRequirements(&$errors = [], &$warnings = [])
+    {
+        /** @var Redis $cache */
+        $cache = \XF::app()->cache();
+        if (!($cache instanceof Redis) || !($credis = $cache->getCredis(false)))
+        {
+            $errors[] = 'This add-on requires Redis Cache to be installed and configured';
+        }
+    }
+}
